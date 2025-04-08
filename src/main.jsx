d537887c131fd667599e9849fa0fdad8e21c262f
ReiKoka <reikoka1@outlook.com>
2025-04-08 17:26:44 +0200

    got changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import App from "./App.jsx";
 import { NavProvider } from "./context/NavContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 import { ToastContainer } from "react-toastify";
-import { ModalProvider } from "./context/ModalContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,9 +13,6 @@ createRoot(document.getElementById("root")).render(
       <CartProvider>
         <App />
         <ToastContainer />
-        <ModalProvider>
-          <App />
-        </ModalProvider>
       </CartProvider>
     </NavProvider>
   </StrictMode>,
