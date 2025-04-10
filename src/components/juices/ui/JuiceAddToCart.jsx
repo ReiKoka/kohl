426ea05b38f1bbd7b34fc6ab3809eb1bf68e89d7
ReiKoka <reikoka1@outlook.com>
2025-04-10 17:28:01 +0200

    added add to cart button

--- src/components/juices/ui/JuiceAddToCart.jsx
+++ src/components/juices/ui/JuiceAddToCart.jsx
@@ -6,27 +6,36 @@ function JuiceAddToCart({ product }) {
   const [quantity, setQuantity] = useState(1);
 
   return (
-    <div className="mt-auto flex w-full items-center justify-between p-4">
-      <p className="text-secondary flex items-center gap-2 text-xl">
-        <Money className="fill-secondary w-8 h-8 " />
-        {formatPriceALL(product.price)}
-      </p>
+    <div className="mt-auto flex w-full flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <p className="text-secondary text-xl">
+          {formatPriceALL(product.price)}
+        </p>
 
-      <div className="flex max-w-min items-center gap-4">
-        <button
-          className="btn btn-sm bg-primary active:animate-jump aspect-square rounded-full p-0"
-          onClick={() => quantity > 1 && setQuantity((prev) => prev - 1)}
-        >
-          <Minus className="fill-base-100 h-5 w-5" />
-        </button>
-        <p className="text-base-content font-primary text-base">{quantity}</p>
-        <button
-          className="btn btn-sm bg-primary active:animate-jump aspect-square rounded-full p-0"
-          onClick={() => setQuantity((prev) => prev + 1)}
-        >
-          <Plus className="fill-base-100 h-5 w-5" />
-        </button>
+        <div className="flex items-center gap-2">
+          <button
+            className="btn btn-sm bg-primary active:animate-jump aspect-square rounded-full p-0"
+            onClick={() => quantity > 1 && setQuantity((prev) => prev - 1)}
+          >
+            <Minus className="fill-base-100 h-4 w-4" />
+          </button>
+          <div className="w-6 text-center">
+            <p className="text-base-content font-primary text-lg font-medium">
+              {quantity}
+            </p>
+          </div>
+          <button
+            className="btn btn-sm bg-primary active:animate-jump aspect-square rounded-full p-0"
+            onClick={() => setQuantity((prev) => prev + 1)}
+          >
+            <Plus className="fill-base-100 h-4 w-4" />
+          </button>
+        </div>
       </div>
+
+      <button className="btn btn-primary btn-lg font-primary active:animate-jump">
+        Add to Cart
+      </button>
     </div>
   );
 }
