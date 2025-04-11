89955f6aa46027f9190a645c4dd09e8cba774249
ReiKoka <reikoka1@outlook.com>
2025-04-11 18:03:57 +0200

    fixed button position on cart page

--- src/components/cart/FilledCart.jsx
+++ src/components/cart/FilledCart.jsx
@@ -4,14 +4,19 @@ import SingleCartItem from "./SingleCartItem";
 function FilledCart() {
   const { cart, setCart } = useCart();
   return (
-    <div className="flex h-full flex-col gap-4 p-2">
+    <div className="flex h-full flex-col gap-4 overflow-hidden">
       <h1 className="font-secondary text-secondary my-2 text-center text-2xl font-normal capitalize md:text-3xl lg:text-4xl">
         Your Cart
       </h1>
-      <div className="relative my-3 h-full grow overflow-y-auto">
+      <div className="my-3 h-full grow overflow-y-auto">
         {cart.map((item, index) => (
           <SingleCartItem key={index} item={item} />
         ))}
+      </div>
+      <div className="bg-accent absolute bottom-0 left-0 z-10 w-full rounded-t-2xl p-4">
+        <button className="btn btn-primary font-primary w-full text-lg">
+          Checkout
+        </button>
       </div>
     </div>
   );
